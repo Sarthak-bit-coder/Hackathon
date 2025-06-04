10f6c3fa5adb22f65f00b6d536e516a9f7a5f0c7
alokj <ayeshajena9@gmail.com>
2025-06-04 16:54:50 -0700

    p9y8bp89

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# In-memory storage for reports
+# In-memory list of reports
 reports = []
 
 @app.route('/')
@@ -13,6 +13,10 @@ def home():
 
 @app.route('/api/reports', methods=['GET'])
 def get_reports():
+    zip_code = request.args.get('zip')
+    if zip_code:
+        filtered = [r for r in reports if r['zip'] == zip_code]
+        return jsonify(filtered)
     return jsonify(reports)
 
 @app.route('/api/reports', methods=['POST'])
@@ -20,7 +24,8 @@ def post_report():
     data = request.json
     report = {
         "type": data.get("type"),
-        "description": data.get("description")
+        "description": data.get("description"),
+        "zip": data.get("zip")
     }
     reports.append(report)
     return jsonify(report), 201
